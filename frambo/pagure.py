f868a1375e1f072ecab61877c7d2cfe5d49fcf51
Petr "Stone" Hracek <phracek@redhat.com>
2020-03-16 11:15:46 +0100

    Fixes #17 Initiate PAGURE_PORT to empty str
    In case of port is not defined PAGURE_PORT is initiate
    to empty str.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- frambo/pagure.py
+++ frambo/pagure.py
@@ -1,7 +1,12 @@
 from frambo.config import get_from_frambo_config
 
 PAGURE_HOST = get_from_frambo_config("pagure", "host")
-PAGURE_PORT = get_from_frambo_config("pagure", "port")
+
+try:
+    PAGURE_PORT = get_from_frambo_config("pagure", "port")
+except ValueError:
+    PAGURE_PORT = ""
+
 PAGURE_URL = f"https://{PAGURE_HOST}/"
 
 
